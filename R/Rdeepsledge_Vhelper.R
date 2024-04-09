f7039a3c515a4b16adea7f08e6efa434e33ce027
Robin <Frederic.Robin@dfo-mpo.gc.ca>
2024-04-09 00:05:47 -0400

    helper

---
+++ R/Rdeepsledge_Vhelper.R
@@ -0,0 +1,8 @@
+#' Ouvrir la vignette de l'aide du package
+#'
+#' Ouvre la vignette de l'aide du package Rdeepsledge.
+#' @export
+#'
+open_help_vignette <- function() {
+  utils::browseVignettes("Rdeepsledge")
+}
